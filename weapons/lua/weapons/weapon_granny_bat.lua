2be2b745c58e7ca99cc4ff7873efb26b79437054
boglerishere <txebogler@gmail.com>
2024-10-03 17:29:01 +0300

    Granny weapon fix

--- weapons/lua/weapons/weapon_granny_bat.lua
+++ weapons/lua/weapons/weapon_granny_bat.lua
@@ -1,13 +1,10 @@
-
-
-
 SWEP.PrintName = "Деревянная бита Грени"
 
 SWEP.Category = "Ближний Бой"
 SWEP.Instructions = "Часть спортивного инвентаря, предназначенная для нанесения ударов по мячу. Также популярно как холодное оружие благодаря своему удобству. Особенности конструкции биты позволяют наносить ею тяжёлые и мощные удары."
 
-SWEP.Spawnable= true
-SWEP.AdminSpawnable= true
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
 SWEP.AdminOnly = false
 
 SWEP.ViewModelFOV = 60
@@ -29,225 +26,153 @@ SWEP.FiresUnderwater = false
 
 SWEP.DrawCrosshair = false
 
-SWEP.DrawAmmo = true
+SWEP.DrawAmmo = false
 
 SWEP.Base = "weapon_base"
 
-SWEP.Primary.Sound = Sound( "Weapon_Knife.Single" )
-SWEP.Primary.Damage = 90
+SWEP.Primary.Sound = Sound("Weapon_Crowbar.Single")
+SWEP.Primary.Damage = 35
 SWEP.Primary.Ammo = "none"
-SWEP.Primary.DefaultClip = 0
+SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = true
 SWEP.Primary.Recoil = 0.5
-SWEP.Primary.Delay = 1.1
+SWEP.Primary.Delay = 0.5
 SWEP.Primary.Force = 150
 
-SWEP.Secondary.ClipSize = 0
-SWEP.Secondary.DefaultClip = 0
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
 
-function SWEP:DrawWeaponSelection( x, y, wide, tall, alpha )
-	if !IsValid(DrawModel) then
-		DrawModel = ClientsideModel( self.WorldModel, RENDER_GROUP_OPAQUE_ENTITY );
-		DrawModel:SetNoDraw( true );
-	else
-		DrawModel:SetModel( self.WorldModel )
-
-		local vec = Vector(55,55,55)
-		local ang = Vector(-48,-48,-48):Angle()
-
-		cam.Start3D( vec, ang, 20, x, y+35, wide, tall, 5, 4096 )
-			cam.IgnoreZ( true )
-			render.SuppressEngineLighting( true )
-
-			render.SetLightingOrigin( self:GetPos() )
-			render.ResetModelLighting( 50/255, 50/255, 50/255 )
-			render.SetColorModulation( 1, 1, 1 )
-			render.SetBlend( 255 )
-
-			render.SetModelLighting( 4, 1, 1, 1 )
-
-			DrawModel:SetRenderAngles( Angle( 0, RealTime() * 30 % 360, 0 ) )
-			DrawModel:DrawModel()
-			DrawModel:SetRenderAngles()
-
-			render.SetColorModulation( 1, 1, 1 )
-			render.SetBlend( 1 )
-			render.SuppressEngineLighting( false )
-			cam.IgnoreZ( false )
-		cam.End3D()
-	end
-
-	self:PrintWeaponInfo( x + wide + 20, y + tall * 0.95, alpha )
-
-end
-
-function Circle( x, y, radius, seg )
-    local cir = {}
-
-    table.insert( cir, { x = x, y = y, u = 0.5, v = 0.5 } )
-    for i = 0, seg do
-        local a = math.rad( ( i / seg ) * -360 )
-        table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
-    end
-
-    local a = math.rad( 0 ) -- This is needed for non absolute segment counts
-    table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
-
-    surface.DrawPoly( cir )
-end
-
-function SWEP:DrawHUD()
-		if not (GetViewEntity() == LocalPlayer()) then return end
-		if LocalPlayer():InVehicle() then return end
-			local ply = self:GetOwner()
-local t = {}
-t.start = ply:GetAttachment(ply:LookupAttachment("eyes")).Pos
-t.endpos = t.start + ply:GetAngles():Forward() * 90
-t.filter = self:GetOwner()
-local Tr = util.TraceLine(t)
-
-if Tr.Hit then
-
-		local Size = math.Clamp(1 - ((Tr.HitPos - self:GetOwner():GetShootPos()):Length() / 90) ^ 2, .1, .3)
-		surface.SetDrawColor(Color(200, 200, 200, 200))
-		draw.NoTexture()
-		Circle(Tr.HitPos:ToScreen().x, Tr.HitPos:ToScreen().y, 55 * Size, 32)
-
-		surface.SetDrawColor(Color(255, 255, 255, 200))
-		draw.NoTexture()
-		Circle(Tr.HitPos:ToScreen().x, Tr.HitPos:ToScreen().y, 40 * Size, 32)
-	end
-end
-
-if CLIENT then
-	local WorldModel = ClientsideModel(SWEP.WorldModel)
-
-	-- Settings...
-	WorldModel:SetSkin(1)
-	WorldModel:SetNoDraw(true)
-
-	function SWEP:DrawWorldModel()
-		local _Owner = self:GetOwner()
-
-		if (IsValid(_Owner)) then
-            -- Specify a good position
-			local offsetVec = Vector(8,-1,0)
-			local offsetAng = Angle(150, 0, 0)
-			
-			local boneid = _Owner:LookupBone("ValveBiped.Bip01_R_Hand") -- Right Hand
-			if !boneid then return end
-
-			local matrix = _Owner:GetBoneMatrix(boneid)
-			if !matrix then return end
-
-			local newPos, newAng = LocalToWorld(offsetVec, offsetAng, matrix:GetTranslation(), matrix:GetAngles())
-
-			WorldModel:SetPos(newPos)
-			WorldModel:SetAngles(newAng)
-			WorldModel:SetModelScale(1.3)
-
-            WorldModel:SetupBones()
-		else
-			WorldModel:SetPos(self:GetPos())
-			WorldModel:SetAngles(self:GetAngles())
-		end
-
-		WorldModel:DrawModel()
-	end
-end
+SWEP.MeleeReach = 48
+SWEP.MeleeSize = 4.5
+SWEP.MeleeDamageType = DMG_CLUB
 
 function SWEP:Initialize()
-self:SetHoldType( "melee2" )
+    self:SetHoldType(self.HoldType)
 end
 
-
 function SWEP:Deploy()
-self:SetNextPrimaryFire( CurTime() + self:GetOwner():GetViewModel():SequenceDuration() )
-self:SetHoldType( "melee2" )
+    self:SendWeaponAnim(ACT_VM_DRAW)
+    self:SetNextPrimaryFire(CurTime() + self:SequenceDuration())
+    return true
 end
 
 function SWEP:Holster()
-return true
+    return true
 end
 
 function SWEP:PrimaryAttack()
-	self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
-	self:SetNextPrimaryFire( CurTime() + self.Primary.Delay/((self:GetOwner().stamina or 100)/100)-(self:GetOwner():GetNWInt("Adrenaline")/5) )
+    self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 
-	if SERVER then
-		self:GetOwner():EmitSound( "weapons/slam/throw.wav",60 )
-		self:GetOwner().stamina = math.max(self:GetOwner().stamina - 8,0)
-	end
-	self:GetOwner():LagCompensation( true )
-	local ply = self:GetOwner()
+    local owner = self:GetOwner()
+    owner:SetAnimation(PLAYER_ATTACK1)
+    self:SendWeaponAnim(ACT_VM_HITCENTER)
 
-	local tra = {}
-	tra.start = ply:GetAttachment(ply:LookupAttachment("eyes")).Pos
-	tra.endpos = tra.start + ply:GetAngles():Forward() * 80
-	tra.filter = self:GetOwner()
-	local Tr = util.TraceLine(tra)
-	local t = {}
-	local pos1, pos2
-	local tr
-	if not Tr.Hit then
-		t.start = ply:GetAttachment(ply:LookupAttachment("eyes")).Pos
-		t.endpos = t.start + ply:GetAngles():Forward() * 80
-		t.filter = function(ent) return ent ~= self:GetOwner() and (ent:IsPlayer() or ent:IsRagdoll()) end
-		t.mins = -Vector(10,10,10)
-		t.maxs = Vector(10,10,10)
-		tr = util.TraceHull(t)
-	else
-		tr = util.TraceLine(tra)
-	end
+    if SERVER then
+        owner:EmitSound(self.Primary.Sound)
+    end
 
-	pos1 = tr.HitPos + tr.HitNormal
-	pos2 = tr.HitPos - tr.HitNormal
-	if true then
-		if SERVER and tr.HitWorld then
-			self:GetOwner():EmitSound(  "physics/wood/wood_plank_impact_hard"..math.random(1,5)..".wav",60  )
-		end
+    owner:LagCompensation(true)
+    
+    local tr = util.TraceLine({
+        start = owner:GetShootPos(),
+        endpos = owner:GetShootPos() + owner:GetAimVector() * self.MeleeReach,
+        filter = owner,
+        mask = MASK_SHOT_HULL
+    })
 
-		if IsValid( tr.Entity ) and SERVER then
-			local dmginfo = DamageInfo()
-			dmginfo:SetDamageType( DMG_CLUB )
-			dmginfo:SetAttacker( self:GetOwner() )
-			dmginfo:SetInflictor( self )
-			dmginfo:SetDamagePosition( tr.HitPos )
-			dmginfo:SetDamageForce( self:GetOwner():GetForward() * self.Primary.Force )
-			local angle = self:GetOwner():GetAngles().y - tr.Entity:GetAngles().y
-			if angle < -180 then angle = 360 + angle end
+    if not IsValid(tr.Entity) then
+        tr = util.TraceHull({
+            start = owner:GetShootPos(),
+            endpos = owner:GetShootPos() + owner:GetAimVector() * self.MeleeReach,
+            filter = owner,
+            mins = Vector(-self.MeleeSize, -self.MeleeSize, -self.MeleeSize),
+            maxs = Vector(self.MeleeSize, self.MeleeSize, self.MeleeSize),
+            mask = MASK_SHOT_HULL
+        })
+    end
 
-			if angle <= 90 and angle >= -90 then
-				dmginfo:SetDamage( self.Primary.Damage * 1.5 )
-			else
-				dmginfo:SetDamage( self.Primary.Damage / 1.5 )
-			end
+    if tr.Hit then
+        if SERVER then
+            local dmginfo = DamageInfo()
+            dmginfo:SetDamage(self.Primary.Damage)
+            dmginfo:SetDamageType(self.MeleeDamageType)
+            dmginfo:SetAttacker(owner)
+            dmginfo:SetInflictor(self)
+            dmginfo:SetDamageForce(owner:GetAimVector() * self.Primary.Force)
+            dmginfo:SetDamagePosition(tr.HitPos)
 
-			if tr.Entity:IsNPC() or tr.Entity:IsPlayer() then
-				self:GetOwner():EmitSound( "Flesh.ImpactHard",60 )
-			else
+            tr.Entity:TakeDamageInfo(dmginfo)
 
-				if tr.Entity:GetClass() == "prop_ragdoll" then
-					self:GetOwner():EmitSound(  "Flesh.ImpactHard",60  )
-				else
-					self:GetOwner():EmitSound(  "physics/wood/wood_plank_impact_hard"..math.random(1,5)..".wav",60  )
-				end
+            if tr.Entity:IsPlayer() then
+                self:ApplyPlayerKnockback(tr.Entity, dmginfo)
+            else
+                self:ApplyEntityKnockback(tr.Entity, dmginfo)
+            end
 
-			end
-			tr.Entity:TakeDamageInfo( dmginfo )
-		end
-	end
+            if tr.Entity:IsNPC() or tr.Entity:IsPlayer() then
+                owner:EmitSound("Flesh.ImpactHard")
+            else
+                owner:EmitSound("Weapon_Crowbar.Melee_Hit")
+            end
+        end
+    else
+        if SERVER then
+            owner:EmitSound("Weapon_Crowbar.Single")
+        end
+    end
 
-	self:GetOwner():LagCompensation( false )
+    owner:LagCompensation(false)
 end
 
 function SWEP:SecondaryAttack()
+    -- No secondary attack
 end
 
-function SWEP:Reload()
+function SWEP:ApplyPlayerKnockback(ent, dmginfo)
+    local knockbackForce = self:GetOwner():GetAimVector() * (dmginfo:GetDamage() * 500)
+    knockbackForce.z = math.max(knockbackForce.z, 100) -- Ensure some upward force
+    
+    ent:SetVelocity(knockbackForce)
+    
+    -- Apply view punch
+    local viewPunch = Angle(-dmginfo:GetDamage() * 0.5, 0, 0)
+    ent:ViewPunch(viewPunch)
+end
+function SWEP:ApplyEntityKnockback(ent, dmginfo)
+    local phys = ent:GetPhysicsObject()
+    if phys:IsValid() and phys:IsMoveable() then
+        phys:ApplyForceCenter(dmginfo:GetDamageForce())
+        ent:SetPhysicsAttacker(self:GetOwner())
+    end
 end
 
-function SWEP:Think()
+function SWEP:DrawHUD()
+    if not (GetViewEntity() == LocalPlayer()) then return end
+    if LocalPlayer():InVehicle() then return end
+
+    local tr = util.TraceLine({
+        start = self:GetOwner():GetShootPos(),
+        endpos = self:GetOwner():GetShootPos() + self:GetOwner():GetAimVector() * self.MeleeReach,
+        filter = self:GetOwner()
+    })
+
+    if tr.Hit then
+        local size = math.Clamp(1 - ((tr.HitPos - self:GetOwner():GetShootPos()):Length() / self.MeleeReach) ^ 2, .1, .3)
+        surface.SetDrawColor(Color(255, 255, 255, 200))
+        surface.DrawCircle(tr.HitPos:ToScreen().x, tr.HitPos:ToScreen().y, 55 * size)
+        surface.SetDrawColor(Color(255, 0, 0, 200))
+        surface.DrawCircle(tr.HitPos:ToScreen().x, tr.HitPos:ToScreen().y, 40 * size)
+    end
+end
+
+if CLIENT then
+    function SWEP:DrawWorldModel()
+        self:DrawModel()
+    end
+
+    function SWEP:DrawWorldModelTranslucent()
+        self:DrawModel()
+    end
 end
