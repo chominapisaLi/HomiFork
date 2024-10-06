e1086a5f16b87b3f0af6fc8ce859167caa6e32a9
Covyshka <vadimpavlygen@gmail.com>
2024-10-06 14:13:03 +0300

    Update init_tier_0_sh.lua

--- homigrad/gamemodes/homigrad/gamemode/game/levels/One_traitor/init_tier_0_sh.lua
+++ homigrad/gamemodes/homigrad/gamemode/game/levels/One_traitor/init_tier_0_sh.lua
@@ -1,23 +1,23 @@
-table.insert(LevelList,"homicide")
-homicide = homicide or {}
-homicide.Name = "Homicide"
+table.insert(LevelList,"oneinnocent")
+oneinnocent = oneinnocent or {}
+oneinnocent.Name = "Иголка в стоге сена"
 
-homicide.red = {"Невиновный",Color(125,125,125),
+oneinnocent.red = {"Невиновный",Color(125,125,125),
     models = tdm.models
 }
 
-homicide.teamEncoder = {
+oneinnocent.teamEncoder = {
     [1] = "red"
 }
 
-homicide.RoundRandomDefalut = 3
+oneinnocent.RoundRandomDefalut = 3
 
 local playsound = false
 if SERVER then
     util.AddNetworkString("roundType")
 else
     net.Receive("roundType",function(len)
-        homicide.roundType = net.ReadInt(4)
+        oneinnocent.roundType = net.ReadInt(4)
         playsound = true
     end)
 end
@@ -29,9 +29,9 @@ end
     ["gun-free-zone"] = 3
 }--]]
 
-local homicide_setmode = CreateConVar("homicide_setmode","",FCVAR_LUA_SERVER,"")
+local oneinnocent_setmode = CreateConVar("homicide_setmode","",FCVAR_LUA_SERVER,"")
 
-function homicide.IsMapBig()
+function oneinnocent.IsMapBig()
     local mins,maxs = game.GetWorld():GetModelBounds()
     local skybox = 0
     for i,ent in pairs(ents.FindByClass("sky_camera")) do
@@ -46,19 +46,19 @@ function homicide.IsMapBig()
     --Vector(-10000, -2000, -2500) Vector(5000, 10000, 800)
 end
 
-function homicide.StartRound(data)
-    team.SetColor(1,homicide.red[2])
+function oneinnocent.StartRound(data)
+    team.SetColor(1,oneinnocent.red[2])
 
     game.CleanUpMap(false)
 
     if SERVER then
-        local roundType = homicide_setmode:GetInt() == 1 and 1 or (homicide.IsMapBig() and 1) or false
+        local roundType = oneinnocent_setmode:GetInt() == 1 and 1 or (oneinnocent.IsMapBig() and 1) or false
 
-        homicide.roundType = roundType or math.random(2,4)
+        oneinnocent.roundType = roundType or math.random(2,4)
         --soe, standard, gun-free-zone, wild west
         --print(homicide_setmode:GetString(),homicide.roundType)
         net.Start("roundType")
-        net.WriteInt(homicide.roundType,4)
+        net.WriteInt(oneinnocent.roundType,4)
         net.Broadcast()
     end
 
@@ -74,20 +74,20 @@ function homicide.StartRound(data)
         return
     end
 
-    return homicide.StartRoundSV()
+    return oneinnocent.StartRoundSV()
 end
 
 if SERVER then return end
 
 local red,blue = Color(200,0,10),Color(75,75,255)
 local gray = Color(122,122,122,255)
-function homicide.GetTeamName(ply)
-    if ply.roleT then return "Предатель",red end
-    if ply.roleCT then return "Невиновный",blue end
+function oneinnocent.GetTeamName(ply)
+    if ply.roleT then return "Невиновный",red end
+    if ply.roleCT then return "Предатель",blue end
 
     local teamID = ply:Team()
     if teamID == 1 then
-        return "Невиновный",ScoreboardSpec
+        return "Предатель",ScoreboardSpec
     end
     if teamID == 3 then
         return "Спецназ",blue
@@ -103,12 +103,12 @@ net.Receive("homicide_roleget",function()
     for i,ply in pairs(role[2]) do ply.roleCT = true end
 end)
 
-function homicide.HUDPaint_Spectate(spec)
+function oneinnocent.HUDPaint_Spectate(spec)
     --local name,color = homicide.GetTeamName(spec)
     --draw.SimpleText(name,"HomigradFontBig",ScrW() / 2,ScrH() - 150,color,TEXT_ALIGN_CENTER)
 end
 
-function homicide.Scoreboard_Status(ply)
+function oneinnocent.Scoreboard_Status(ply)
     local lply = LocalPlayer()
     if not lply:Alive() or lply:Team() == 1002 then return true end
 
@@ -119,16 +119,16 @@ local red,blue = Color(200,0,10),Color(75,75,255)
 local roundTypes = {"State of Emergency", "Standard", "Gun-Free-Zone", "Wild West"}
 local roundSound = {"snd_jack_hmcd_disaster.mp3","snd_jack_hmcd_shining.mp3","snd_jack_hmcd_panic.mp3","snd_jack_hmcd_wildwest.mp3"}
 
-function homicide.HUDPaint_RoundLeft(white2)
-    local roundType = homicide.roundType or 2
+function oneinnocent.HUDPaint_RoundLeft(white2)
+    local roundType = oneinnocent.roundType or 2
     local lply = LocalPlayer()
-    local name,color = homicide.GetTeamName(lply)
+    local name,color = oneinnocent.GetTeamName(lply)
 
     local startRound = roundTimeStart + 7 - CurTime()
     if startRound > 0 and lply:Alive() then
         if playsound then
             playsound = false
-            surface.PlaySound(roundSound[homicide.roundType])
+            surface.PlaySound(roundSound[oneinnocent.roundType])
         end
         lply:ScreenFade(SCREENFADE.IN,Color(0,0,0,255),3,0.5)
 
@@ -143,15 +143,15 @@ function homicide.HUDPaint_RoundLeft(white2)
         draw.DrawText( roundTypes[roundType], "HomigradFontBig", ScrW() / 2, ScrH() / 5, Color( 55,55,155,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
 
         if lply.roleT then
-            draw.DrawText( "Ваша задача убить всех до прибытия полиции", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 155,55,55,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
+            draw.DrawText( "Ваша задача выжить или убить всех до прибытия полиции", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 155,55,55,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
         elseif lply.roleCT then
-            if homicide.roundType == 2 then 
-                draw.DrawText( "У вас есть крупногабаритное оружие, постарайтесь нейтрализовать предателя", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,155,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
+            if oneinnocent.roundType == 2 then 
+                draw.DrawText( "У вас есть крупногабаритное оружие, постарайтесь нейтрализовать невиного", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,155,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
             else
-                draw.DrawText( "У вас есть скрытое огнестрельное оружие, постарайтесь нейтрализовать предателя", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,155,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
+                draw.DrawText( "У вас есть скрытое огнестрельное оружие, постарайтесь нейтрализовать невиного", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,155,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
             end
         else
-            draw.DrawText( "Найдите предателя, свяжите или убейте его для победы. Не доверяйте никому...", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,55,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
+            draw.DrawText( "Найдите невиновного, свяжите или убейте его для победы. Не доверяйте никому...", "HomigradFontBig", ScrW() / 2, ScrH() / 1.2, Color( 55,55,55,math.Clamp(startRound - 0.5,0,1) * 255 ), TEXT_ALIGN_CENTER )
         end
         return
     end
@@ -183,7 +183,7 @@ function homicide.HUDPaint_RoundLeft(white2)
     end
 end
 
-function homicide.VBWHide(ply,list)
+function oneinnocent.VBWHide(ply,list)
     if (not ply:IsRagdoll() and ply:Team() == 1002) then return end
 
     local blad = {}
@@ -199,8 +199,8 @@ function homicide.VBWHide(ply,list)
     return blad
 end
 
-function homicide.Scoreboard_DrawLast(ply)
+function oneinnocent.Scoreboard_DrawLast(ply)
     if LocalPlayer():Team() ~= 1002 and LocalPlayer():Alive() then return false end
 end
 
-homicide.SupportCenter = true
+oneinnocent.SupportCenter = true
